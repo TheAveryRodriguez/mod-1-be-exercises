787c9a8252386887c2695299ed1db87e1eff35a5
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-14 11:14:15 -0500

    update the grocery list and suitcase practices

--- lessons/hashes/warm_up/grocery_list_array.rb
+++ lessons/hashes/warm_up/grocery_list_array.rb
@@ -1,11 +1,24 @@
-fridge_items_1 = ["milk", "eggs", "eggs","eggs", "eggs",
-                  "eggs", "eggs", "avocado", "avocado", "tortilla",
-                  "tortilla", "tortilla", "tortilla", "tortilla",
-                  "tortilla", "tortilla", "tortilla", "tortilla"]
+fridge_items_1 = ["milk", "eggs", "eggs", "eggs", "eggs",
+  "eggs", "eggs", "avocado", "avocado", "tortilla",
+  "tortilla", "tortilla", "tortilla", "tortilla",
+  "tortilla", "tortilla", "tortilla", "tortilla"]
 
 # Looking at the collection above:
 
 # 1. What is problematic about listing `fridge_items_1`
 # as an array structured grocery list?
 
+# It would be easier to have a variable for each "type" of item.
+# You would need to create an additional code to iterate over the array to determine how many of each to receive.
+# Ordering matters when using an array.
+# Thigns can get unorganized inside
+
 # 2. What might be a more readable way to structure this?
+# Im assumign a hash :eggs :milk :tortilla :avocado???
+# Using some sort of key value pair: key is item and value is quantity
+# A nested array, put all produce together, dairy, etc, etc
+## fridge_items_1 = [
+# []
+# []
+# []
+# ]
--- lessons/hashes/warm_up/suitcase_hash.rb
+++ lessons/hashes/warm_up/suitcase_hash.rb
@@ -1,8 +1,29 @@
+# suitcase = Hash.new(0)
+## Instance of a Hash
+### Default value == 0
+
 suitcase = {
   "socks" => 4,
-  "pants" => 1,
+  "pants" => 1
 }
 
+suitcase ["shirts"] = 3
+
 # 1. Add a key/value pair of shirts/3 to the suitcase.
+## Grab the hash, set the key ["shirts"] and assign it a value 3
+### Key always goes inside of [], matching key types is preferred
+
 # 2. Increase the value of shirts by 1.
+## suitcase["shirts"] = 4   or   suitcase["shirts"] += 1
+### If you dont know values prior, doing the += 1 is accomplishing what you want it to accomplish
+
 # 3. Add a key/value pair of swimsuit/true to the suitcase.
+## suitcase["swimsuit"] = true
+
+# 4. How to delete a key?
+## suitcase.delete("swimsuit")
+
+# EXTRA. if you call a key that doesnt exist, it returns nil
+## Setting a default value to 0 instead of nil for all keys
+###  suitcase.default = 0
+### if key value = nil you cannot +/- the value (nil + 1 = error)
